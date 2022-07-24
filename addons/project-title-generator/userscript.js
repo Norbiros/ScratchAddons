6f7c01a1ebbffe6e0412ac99c1ea07f1c34dd299
Oreg Sam <101597941+OregSamSas@users.noreply.github.com>
2022-07-24 17:28:50 +0200

    Update userscript.js

--- addons/project-title-generator/userscript.js
+++ addons/project-title-generator/userscript.js
@@ -1,12 +1,9 @@
 export default async function ({ addon, global, console, msg }) {
   let pendingReplacement = false;
-
   let ADJECTIVES;
   let NOUNS;
   let FIRSTADJECTIVES;
-
   genWords(addon.auth.scratchLang);
-
   let reduxAvailable = Boolean(addon.tab.redux.state);
   while (!reduxAvailable) {
     await new Promise((resolve) => {
@@ -16,23 +13,19 @@ export default async function ({ addon, global, console, msg }) {
       }, 0);
     });
   }
-
   addon.tab.redux.initialize();
   let isFileUpload = false;
   addon.tab.redux.addEventListener("statechanged", async (e) => {
     if (e.detail.action.type === "scratch-gui/project-state/DONE_LOADING_VM_WITHOUT_ID") {
       // Current loadingState is SHOWING_WITHOUT_ID
-
       if (pendingReplacement) {
         // Never happens AFAIK
         console.log("Pending replacement");
         return;
       }
       pendingReplacement = true;
-
       let expired = false; // So that nothing goes catastrophically wrong
       setTimeout(() => (expired = true), 10000);
-
       const isLoggedIn = await addon.auth.fetchIsLoggedIn();
       if (isLoggedIn) {
         await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "CREATING_NEW");
@@ -41,7 +34,6 @@ export default async function ({ addon, global, console, msg }) {
         await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID");
         // By this point, vanilla new project was saved to cloud
       }
-
       if (addon.settings.get("auto-on-create") && !expired && !isFileUpload) setProjectName();
       pendingReplacement = false;
       isFileUpload = false;
@@ -50,11 +42,9 @@ export default async function ({ addon, global, console, msg }) {
       isFileUpload = true;
     }
   });
-
   // Create the randomizer button
   let button;
   createButton();
-
   addon.tab.redux.addEventListener("statechanged", async (e) => {
     if (e.detail.action.type === "projectTitle/SET_PROJECT_TITLE") {
       let showButton = await shouldButtonShow();
@@ -78,7 +68,6 @@ export default async function ({ addon, global, console, msg }) {
     if (showButton) createButton();
     else removeButton();
   });
-
   async function shouldButtonShow() {
     let currentName = await addon.tab.redux.state.scratchGui.projectTitle;
     if (!addon.settings.get("only-untitled") || currentName === "" || currentName.includes("Untitled")) {
@@ -87,7 +76,6 @@ export default async function ({ addon, global, console, msg }) {
       return false;
     }
   }
-
   async function createButton() {
     let showButton = await shouldButtonShow();
     if (!showButton || document.querySelector("#sa-project-title-generator-button") !== null) {
@@ -112,16 +100,15 @@ export default async function ({ addon, global, console, msg }) {
     nameField.after(button);
     button.addEventListener("click", () => setProjectName());
   }
-
   async function removeButton() {
     let nameContainer = await addon.tab.waitForElement('[class*="menu-bar_menu-bar-item"][class*="menu-bar_growable"]');
     nameContainer.classList.remove("sa-project-title-generator");
     button.remove();
   }
-
   async function setProjectName() {
-    let adj1 = ADJECTIVES[randi(ADJECTIVES.length)];
-    let adj2 = ADJECTIVES[randi(ADJECTIVES.length)];
+    let adj1 = randomAdj();
+    //the chance of being the second adjective blank grows by the length of the first adjective
+    let adj2 = randomAdj(Math.floor(100/adj1.length), adj1);
     let noun1 = NOUNS[randi(NOUNS.length)];
     let newName = `${adj1} ${adj2} ${noun1}`;
     if (FIRSTADJECTIVES === false) {
@@ -130,10 +117,28 @@ export default async function ({ addon, global, console, msg }) {
     addon.tab.redux.dispatch({ type: "projectTitle/SET_PROJECT_TITLE", title: newName });
   }
 
+  function randomAdj(blankChance=30, prevAdj="") {
+    // console.log("1:" + JSON.stringify(blankChance))
+    if (randi(blankChance) < 2) {
+      //The adjective will be blank by some chance
+      return "";
+    } else {
+      let adj = ADJECTIVES[randi(ADJECTIVES.length)];
+      if (prevAdj !== "") { // if it's the second adjective
+        while (((prevAdj[0] !== adj[0]) && (randi(2) !== 1)) || (adj === prevAdj)) {
+          //if the first letter of the adjectives doesn't match there's some chance, a new one will be generated
+          //Or if the adjectives are the same, it will generates a new too
+          adj = ADJECTIVES[randi(ADJECTIVES.length)];
+        }
+      }
+      return adj;
+    }
+  }
+
   function randi(max) {
     return Math.floor(Math.random() * max);
   }
-
+  
   async function genWords(lang) {
     try {
       ADJECTIVES = (await import("./data/" + lang + ".js")).adjectives;
